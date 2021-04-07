42373b35b1a51f8bc56679bc2344e9bd9019d7b1
Joe Schelske <joe.schelske@gmail.com>
2021-04-06 22:29:14 -0500

    analyze.py begun, index.html created

---
+++ analyze.py
@@ -0,0 +1,8 @@
+import praw
+from textblob import TextBlob
+
+reddit = praw.Reddit(
+    client_id="xt5pmMV2eaKApw",
+    client_secret="Q_u2kTLSRuo2XRIvighrD0JXc1o7Cg",
+    user_agent="reddit-reviews sentiment analysis by u/Lost_You4257",
+)
